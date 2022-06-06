43b5992cfcd29bbfe4bedb12797ae2cf90bc7bf7
anekoGurasu <anekogur@gmail.com>
2022-06-06 19:57:46 +0200

    projects + part of reviews

--- js/script.js
+++ js/script.js
@@ -1,31 +1,49 @@
 
+let projects=[
+    {
+        name: "Portfolio",
+        customer: "AnekoGurasu (me)",
+        url:"anekogurasu.github.io",
+        used: "HTML, CSS (SCSS), JavaScript, ajax",
+        desc: "Portfolio",
+        img:"/img/pfpf.png"
+    },
+    {
+        name: "Portfolio 2",
+        customer: "AnekoGurasu (me)",
+        used: "HTML, CSS (SCSS), JavaScript, ajax",
+        img:"/img/pfpf.png"
+    }
+]
 
-function hoverArrowUp(element) {
-    
-    element.setAttribute('src', 'img/up-blue.png');  
-    
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function unhoverArrowUp(element) {
-    element.setAttribute('src', 'img/50-508007_arrow-of-double-angle-pointing-up-vector-double.png');
-    
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showToolTipFlag() {
-    let tooltip = document.getElementById('tooltip-flag');
-    tooltip.classList.add("tooltip-flag-show");
-}
-function hidToolTipFlag() {
-    let tooltip = document.getElementById('tooltip-flag');
-    tooltip.classList.remove('tooltip-flag-show');
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
 
-function showToolTipHeader() {
-    let tooltip = document.getElementById('tooltip-header');
-    tooltip.classList.add("tooltip-header-show");
-}
-function hidToolTipHeader() {
-    let tooltip = document.getElementById('tooltip-header');
-    tooltip.classList.remove('tooltip-header-show');
-}
+
 
