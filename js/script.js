0e50c58b942fbb17c92022b0c7c70dfc839359cc
anekoGurasu <anekogur@gmail.com>
2022-06-15 22:21:53 +0200

    menu animation

--- js/script.js
+++ js/script.js
@@ -45,5 +45,88 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+function showToolTipFlag(){
+  let tooltip=document.getElementById("tooltip-flag");
+  tooltip.style.opacity=0.75;
+}
+
+function hideToolTipFlag(){
+  let tooltip=document.getElementById("tooltip-flag");
+  tooltip.style.opacity=0;
+}
 
 
+
+function getHeight(el){
+  return el.top;
+}
+
+function underlineMenuItem(){
+
+  const intro = document.getElementById("introductionID");
+  const project = document.getElementById("portfolioID");
+  const review = document.getElementById("reviewsID");
+  const contact = document.getElementById("contactID");
+  
+    if($(this).scrollTop() >= $('portfolioID').offset().top - 50) {
+       console.log("ano");
+    } else {
+      console.log("ne");
+    }
+
+}
+
+  const intro = document.getElementById("introductionID");
+  const project = document.getElementById("portfolioID");
+  const review = document.getElementById("reviewsID");
+  const contact = document.getElementById("contactID");
+
+ 
+    $(document).scroll(function() {
+      if (($(this).scrollTop() >= intro.offsetTop)&&($(this).scrollTop() < project.offsetTop)) {
+        $("#nav-underline1").css("border-bottom", "4px solid #3c5c7c"); 
+        $("#a-active1").css("color","#3c5c7c");
+
+        $("#nav-underline2").css("border-bottom", "none");
+        $("#nav-underline3").css("border-bottom", "none"); 
+        $("#nav-underline4").css("border-bottom", "none");  
+
+        $("#a-active2").css("color","#e0e0ff");
+        $("#a-active3").css("color","#e0e0ff");
+        $("#a-active4").css("color","#e0e0ff");
+      } else if(($(this).scrollTop() >= project.offsetTop)&&($(this).scrollTop() < review.offsetTop-10)){
+        $("#nav-underline2").css("border-bottom", "4px solid #3c5c7c"); 
+        $("#a-active2").css("color","#3c5c7c");
+
+        $("#nav-underline1").css("border-bottom", "none");
+        $("#nav-underline3").css("border-bottom", "none"); 
+        $("#nav-underline4").css("border-bottom", "none");  
+
+        $("#a-active1").css("color","#e0e0ff");
+        $("#a-active3").css("color","#e0e0ff");
+        $("#a-active4").css("color","#e0e0ff");
+      } else if(($(this).scrollTop() >= review.offsetTop-10)&&($(this).scrollTop() < contact.offsetTop-10)){
+        $("#nav-underline3").css("border-bottom", "4px solid #3c5c7c"); 
+        $("#a-active3").css("color","#3c5c7c");
+
+        $("#nav-underline1").css("border-bottom", "none");
+        $("#nav-underline2").css("border-bottom", "none"); 
+        $("#nav-underline4").css("border-bottom", "none");  
+
+        $("#a-active1").css("color","#e0e0ff");
+        $("#a-active2").css("color","#e0e0ff");
+        $("#a-active4").css("color","#e0e0ff");
+      }else {
+        $("#nav-underline4").css("border-bottom", "4px solid #3c5c7c"); 
+        $("#a-active4").css("color","#3c5c7c");
+
+        $("#nav-underline1").css("border-bottom", "none");
+        $("#nav-underline2").css("border-bottom", "none"); 
+        $("#nav-underline3").css("border-bottom", "none");  
+
+        $("#a-active1").css("color","#e0e0ff");
+        $("#a-active2").css("color","#e0e0ff");
+        $("#a-active3").css("color","#e0e0ff");
+      }
+    });
+  
