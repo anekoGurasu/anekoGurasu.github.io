d77be6c4c3f6475c8fb62f0231e9fd0382801488
anekoGurasu <anekogur@gmail.com>
2022-11-24 00:41:40 +0100

    About me section 1/2

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showAbout(){
+    $("#content").load("/subpages/about.html");
+}
+function showwebprojects(){
+    $("#content").load("/subpages/websiteprojects.html");
+}
+function showart(){
+    $("#content").load("/subpages/art.html");
+}
