d623dc180e42a5c365087abab014cf1c75e0d27b
anekoGurasu <anekogur@gmail.com>
2026-02-16 20:34:47 +0100

    build fix

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,6 @@ use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use Inertia\Inertia; // <--- TOTO JE NUTNÉ PŘIDAT
 
-// API Routy (tohle je v pořádku)
-Route::get('/api/categories', [CategoryController::class, 'index']);
-Route::get('/api/questions', [QuestionController::class, 'index']);
-Route::get('/api/dashboard', [DashboardController::class, 'index']);
-Route::post('/api/dashboard/save', [DashboardController::class, 'save']);
-
 // Hlavní stránka
 Route::get('/', function () {
     return Inertia::render('Home');
@@ -31,3 +25,9 @@ Route::get('/dashboard', function () {
 Route::get('/login', function () {
     return Inertia::render('Login');
 });
+
+// API Routy (tohle je v pořádku)
+Route::get('/api/categories', [CategoryController::class, 'index']);
+Route::get('/api/questions', [QuestionController::class, 'index']);
+Route::get('/api/dashboard', [DashboardController::class, 'index']);
+Route::post('/api/dashboard/save', [DashboardController::class, 'save']);
